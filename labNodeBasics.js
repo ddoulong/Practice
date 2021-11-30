bd534924adce7e59c4f1c196c7d154744360d8c6
DanRD <danid92@vt.edu>
2021-11-29 23:03:54 -0500

    remembering the basics: npm scripts & nodemon

--- labNodeBasics.js
+++ labNodeBasics.js
@@ -2,6 +2,27 @@ console.log('start');
 const express = require('express');
 const app = express();
 
+app.get('/', (req,res) => {
+    console.log('home route request')
+    res.send('Bunicio && Prince === Love');
+});
+
+app.get('/users', (req,res) => {
+    console.log('/users');
+    res.send([
+    {
+     name: 'Bee',
+     address: 'Unknown',
+     occupation: 'Queen'
+    },
+    {
+     name: 'Sunny',
+     address:'Planet Earth',
+     occupation: 'Professional Snuggler'
+    }
+]);
+});
+
 app.listen(3000, () => {
     console.log('server is up')
 })
